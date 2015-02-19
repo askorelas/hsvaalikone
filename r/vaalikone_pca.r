0b697fe551d90afaa0076c586807bb9c8326725f
Asko <ext-asko.relas@nokia.com>
2015-02-19 14:22:16 +0200

    Small tweaks

--- r/vaalikone_pca.r
+++ r/vaalikone_pca.r
@@ -3,17 +3,17 @@ outputFilePath="OUTPUT_FILE_PATH"
 
 #Read the data in
 vaalikone <- read.csv(file=inputFilePath, head=TRUE, sep=",", encoding="UTF-8")
-values <- vaalikone[2:31]
+questions <- vaalikone[2:31]
 
 #Perform PCA
-vaalikone.pca <- prcomp(values) 
+vaalikone.pca <- prcomp(questions) 
 
 #Define labels and colors for plot
 parties = c("Kristillisdemokraatit",  "Keskusta",  "Kokoomus",	"Muutos 2011",	"Perussuomalaiset",	"SDP",	"SKP",	"Vasemmistoliitto",	"Vihreät",	"Piraattipuolue",	"RKP",	"Itsenäisyyspuolue")
 colors = c(27, 81, 125, 638, 145, 35, 552, 91, 50, 550, 652, 503)
 
 #Plot the first two principal components
-par(mar=c(5.1, 5, 5, 11), xpd=TRUE)
+par(mar=c(5, 5, 5, 11), xpd=TRUE)
 plot(vaalikone.pca$x[,1],vaalikone.pca$x[,2], col=vaalikone$color)
 legend(x=9,y=7,parties,cex=.8, colors)
 
